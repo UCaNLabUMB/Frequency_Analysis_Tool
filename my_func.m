efd0c8266680f96a6a8721d26bca7b363558d879
trungnguyen004 <35817767+trungnguyen004@users.noreply.github.com>
2018-04-12 23:25:57 -0400

    Add files via upload
    Noise Analysis Tool GUI Version 1.1

--- my_func.m
+++ my_func.m
@@ -2,6 +2,6 @@ function [ h ] = my_func(filename)
 m1 = csvread (filename, 22, 0);
 X = fft(m1);
 plot(abs(X));
-title('tek0000CH1');
+title('tek00**CH1');
 xlabel('Frequency(Hz)');
 ylabel('Amplitude');
